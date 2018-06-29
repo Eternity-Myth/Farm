5522b35ccea817cef44d084626123116b816d935
Eternity-Myth <ygnhgwc@163.comm>
2018-06-29 19:31:41 +0800

    答辩完成，整合PPT、报告，存档

--- src/main/sql/test.sql
+++ src/main/sql/test.sql
@@ -49,16 +49,16 @@ CREATE TABLE `consumerinfo` (
   `con_name` varchar(255) DEFAULT NULL COMMENT '客户名',
   `con_state` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `consumerinfo` */
 
 insert  into `consumerinfo`(`id`,`con_phone`,`con_name`,`con_state`) values 
-(5,'1893927089','杨先生',1),
-(6,'13455680324','徐先生',1),
-(7,'13588532209','陈小姐',1),
-(8,'13288920723','林小姐',1),
-(9,'13199273622','苏小姐',1);
+(1,'1893927089','杨先生',1),
+(2,'13455680324','徐先生',1),
+(3,'13588532209','陈小姐',1),
+(4,'13288920723','林小姐',1),
+(5,'13199273622','苏小姐',1);
 
 /*Table structure for table `crops` */
 
@@ -100,16 +100,17 @@ CREATE TABLE `field` (
   `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态',
   `total` float DEFAULT '0' COMMENT '菜地总收益',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='菜地信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='菜地信息表';
 
 /*Data for the table `field` */
 
 insert  into `field`(`id`,`area`,`username`,`start_time`,`time`,`status`,`total`) values 
-(1,132,'杨先生','2018-06-01 23:52:29',12,1,184.702),
-(2,629,'徐先生','2018-06-02 11:42:29',12,1,46.7019),
+(1,120,'杨先生','2018-06-01 23:52:29',11,1,184.702),
+(2,629,'徐先生','2018-06-02 11:42:29',12,1,92.1897),
 (3,145,'林小姐','2018-06-12 00:53:15',12,1,30.5473),
 (4,178,'陈小姐','2018-06-13 11:32:52',6,1,16.3725),
-(5,243,'杨先生','2018-06-16 17:12:06',12,1,52.9232);
+(5,243,'杨先生','2018-06-16 17:12:06',12,1,52.9232),
+(6,50,'杨先生','2018-06-28 13:46:27',12,1,10.323);
 
 /*Table structure for table `items` */
 
@@ -130,13 +131,13 @@ insert  into `items`(`id`,`name`,`quantity`,`unit_price`,`status`) values
 (1,'玉米',6851,1.28,1),
 (2,'水稻',20000,0.33,1),
 (3,'大豆',8675,0.73,1),
-(4,'小麦',18263,0.38,1),
+(4,'小麦',18265,0.38,1),
 (5,'棉花',40000,0.42,1),
-(6,'花生',48270,0.55,1),
+(6,'花生',40000,0.55,1),
 (7,'油菜籽',68770,0.49,1),
-(8,'烤烟',13275,1.12,1),
+(8,'烤烟',5000,1.2,1),
 (9,'甘蔗',5000,2.21,1),
-(10,'柑橘',793,11.27,1);
+(10,'柑橘',780,11.27,1);
 
 /*Table structure for table `log` */
 
@@ -150,7 +151,7 @@ CREATE TABLE `log` (
   `error_msg` varchar(255) DEFAULT NULL COMMENT '错误信息',
   `timestamp` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '时间记录',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=478 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=537 DEFAULT CHARSET=utf8;
 
 /*Data for the table `log` */
 
@@ -626,7 +627,66 @@ insert  into `log`(`id`,`classname`,`method`,`status_code`,`error_msg`,`timestam
 (474,'com.farm.web.PlantController','savaPlant',0,'','2018-06-21 15:06:05'),
 (475,'com.farm.web.PlantController','savaPlant',0,'','2018-06-21 15:06:10'),
 (476,'com.farm.web.PlantController','savaPlant',0,'','2018-06-21 15:06:16'),
-(477,'com.farm.controller.FieldController','updateField',0,'','2018-06-26 00:26:55');
+(477,'com.farm.controller.FieldController','updateField',0,'','2018-06-26 00:26:55'),
+(478,'com.farm.controller.UserController','signIn',0,'','2018-06-28 11:36:13'),
+(479,'com.farm.controller.FieldController','recommendPlan',0,'','2018-06-28 11:36:48'),
+(480,'com.farm.controller.FieldController','recommendPlan',0,'','2018-06-28 11:36:50'),
+(481,'com.farm.controller.FieldController','recommendPlan',0,'','2018-06-28 11:36:52'),
+(482,'com.farm.controller.FieldController','recommendPlan',0,'','2018-06-28 11:36:55'),
+(483,'com.farm.controller.UserController','signIn',0,'','2018-06-28 11:47:25'),
+(484,'com.farm.controller.UserController','signIn',0,'','2018-06-28 11:49:33'),
+(485,'com.farm.controller.UserController','signIn',0,'','2018-06-28 11:50:56'),
+(486,'com.farm.controller.UserController','signIn',0,'','2018-06-28 11:53:00'),
+(487,'com.farm.controller.UserController','signIn',0,'','2018-06-28 12:38:44'),
+(488,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:12'),
+(489,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:21'),
+(490,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:21'),
+(491,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:28'),
+(492,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:28'),
+(493,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:29'),
+(494,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:34'),
+(495,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:34'),
+(496,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:34'),
+(497,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:34'),
+(498,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:41'),
+(499,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:41'),
+(500,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:42'),
+(501,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:42'),
+(502,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:42'),
+(503,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:46'),
+(504,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:46'),
+(505,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:46'),
+(506,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:46'),
+(507,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:47'),
+(508,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:47'),
+(509,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:54'),
+(510,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:54'),
+(511,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:55'),
+(512,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:55'),
+(513,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:55'),
+(514,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:55'),
+(515,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:55'),
+(516,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:39:55'),
+(517,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:04'),
+(518,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:04'),
+(519,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:04'),
+(520,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:04'),
+(521,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:05'),
+(522,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:05'),
+(523,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:05'),
+(524,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:05'),
+(525,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:05'),
+(526,'com.farm.controller.ItemsController','updateItems',0,'','2018-06-28 12:40:38'),
+(527,'com.farm.controller.UserController','signIn',0,'','2018-06-28 13:45:23'),
+(528,'com.farm.controller.FieldController','saveField',0,'','2018-06-28 13:46:27'),
+(529,'com.farm.controller.FieldController','updateField',0,'','2018-06-28 13:46:59'),
+(530,'com.farm.controller.FieldController','recommendPlan',0,'','2018-06-28 13:47:10'),
+(531,'com.farm.controller.FieldController','recommendPlan',0,'','2018-06-28 13:47:13'),
+(532,'com.farm.controller.FieldController','recommendPlan',0,'','2018-06-28 13:47:17'),
+(533,'com.farm.controller.PlantController','savaPlant',0,'','2018-06-28 13:48:30'),
+(534,'com.farm.controller.PlantController','savaPlant',0,'','2018-06-28 13:48:59'),
+(535,'com.farm.controller.PlantController','savaPlant',0,'','2018-06-28 13:49:20'),
+(536,'com.farm.controller.UserController','changeStatus',0,'','2018-06-28 13:52:02');
 
 /*Table structure for table `ordertable` */
 
@@ -647,10 +707,10 @@ CREATE TABLE `ordertable` (
 /*Data for the table `ordertable` */
 
 insert  into `ordertable`(`id`,`user_name`,`item_name`,`quantity`,`total`,`create_time`,`status`) values 
-(1,'杨先生','玉米',577,204.489,'2018-06-16 16:04:13',0),
-(2,'苏小姐','玉米',1344,476.314,'2018-06-16 16:27:47',0),
-(3,'苏小姐','甘蔗',1569,471.484,'2018-06-26 22:08:07',0),
-(4,'苏小姐','大豆',1468,17.4692,'2018-06-26 22:08:17',0);
+(1,'杨先生','玉米',577,738.56,'2018-06-16 16:04:13',0),
+(2,'苏小姐','玉米',1344,1720.32,'2018-06-16 16:27:47',0),
+(3,'苏小姐','甘蔗',1569,3467.49,'2018-06-26 22:08:07',0),
+(4,'苏小姐','大豆',1468,1071.64,'2018-06-26 22:08:17',0);
 
 /*Table structure for table `plant` */
 
@@ -666,7 +726,7 @@ CREATE TABLE `plant` (
   `time` int(3) DEFAULT NULL COMMENT '种植时长',
   `status` tinyint(1) DEFAULT '1' COMMENT '状态',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 /*Data for the table `plant` */
 
@@ -676,7 +736,10 @@ insert  into `plant`(`id`,`fid`,`cname`,`cprofit`,`quantity`,`total`,`time`,`sta
 (3,2,'玉米',0.3177,147,46.7019,3,1),
 (4,3,'大豆',0.0119,2567,30.5473,4,1),
 (5,4,'小麦',0.0025,6549,16.3725,7,1),
-(6,5,'甘蔗',0.3007,176,52.9232,4,1);
+(6,5,'甘蔗',0.3007,176,52.9232,4,1),
+(7,2,'花生',0.0683,666,45.4878,4,1),
+(8,6,'水稻',0.0036,666,2.3976,3,1),
+(9,6,'大豆',0.0119,666,7.9254,4,1);
 
 /*Table structure for table `sellerinfo` */
 
@@ -723,7 +786,7 @@ CREATE TABLE `usertable` (
 
 insert  into `usertable`(`id`,`user_name`,`user_pass`,`user_email`,`register_time`,`status`) values 
 (1,'admin','123456','admin@qq.com','2018-05-23 21:04:04',1),
-(2,'test','123456','test@qq.com','2018-06-10 20:44:31',1);
+(2,'test','123456','test@qq.com','2018-06-10 20:44:31',0);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
